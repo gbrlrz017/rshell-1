993ec0dba986ecc5c93a4da42c7dc80769096538
mxu008 <mxu008@ucr.edu>
2014-11-07 14:23:29 -0800

    cp.cpp modofied

--- src/cp.cpp
+++ src/cp.cpp
@@ -157,19 +157,19 @@ int main(int argc, char** argv)
 	
 	// Check whether the source file is a directory
 	struct stat s;
-    if(stat(argv[1], &s) == 0)
+	if(stat(argv[1], &s) == 0)
 	{
 		if (s.st_mode & S_IFDIR)
 		{
-			cout << "Error: " << argv[1] << " is a directory." << endl;
-		    return 1;
+				cout << "Error: " << argv[1] << " is a directory." << endl;
+				return 1;
 		}
 	}
 	
 	// With optional argument then print out wallclock, user, and system run times for three methods
 	if (argc == 4)
 	{
-		Timer t;
+			Timer t;
 		double time;
 		
 		// Print times for input.get() and output.put()
