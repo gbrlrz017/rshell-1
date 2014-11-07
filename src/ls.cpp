e4b5017d1fece338f739ebc2fbe2483038de3b68
mxu008 <mxu008@ucr.edu>
2014-11-07 00:37:42 -0800

    ls.cpp modified

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,10 +1,29 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-	
+/*
+ * This is a BARE BONES example of how to use opendir/readdir/closedir.  Notice
+ * that there is no error checking on these functions.  You MUST add error 
+ * checking yourself.
+ */
 
-	return 0;
+int main()
+{
+	char *dirName = ".";
+	DIR *dirp = opendir(dirName);
+	dirent *direntp;
+	
+	while ((direntp = readdir(dirp)))
+	{
+		cout << direntp->d_name << endl; // use stat here to find attributes of file
+	}
+	
+	closedir(dirp);
+
+	return;
 }
